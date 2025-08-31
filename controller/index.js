c42cac76ec0405984db11909c1b89a8b0e48613d
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-08-30 23:44:27 -0600

    Define middleware to process request data
    The create function uses the formidable package to parse and validate form data.

--- controller/index.js
+++ controller/index.js
@@ -1,2 +1,27 @@
 const formidable = require("formidable");
 const { create, get, remove } = require("../model/todo");
+
+exports.create = (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, async (err, fields) => {
+    const { description } = fields;
+    // Check to see if the description field exists in the form
+    // If description doesn't exist, send error
+    if (!fields.description) {
+      return res.status(400).json({
+        error: "Description is required.",
+      });
+    }
+    // If description exists, add to database using create() function
+    try {
+      const newTask = await create(description);
+      return res.status(201).send({ data: newTask.rows[0] });
+    } catch (error) {
+      // If description can't be added to database, send error
+      return res.status(400).json({
+        error,
+      });
+    }
+  });
+};

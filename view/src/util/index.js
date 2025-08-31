7c933d16042b3f5914bf880631eaf799c6931243
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-08-31 00:18:34 -0600

    Define POST method in createTodo function

--- view/src/util/index.js
+++ view/src/util/index.js
@@ -1,1 +1,13 @@
 // Create three exported function that will perform the API calls to the Controller
+
+export const createTodo = async (todo) => {
+  try {
+    const res = await fetch("api/todo/create", {
+      method: "POST",
+      body: todo,
+    });
+    return res.json();
+  } catch (error) {
+    return { error };
+  }
+};

dadd8bcf74955d762a97417e9f108f57d9f109fc
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2025-09-01 17:04:18 -0600

    Convert double to single quotes

--- model/todo.js
+++ model/todo.js
@@ -1,17 +1,17 @@
-const pool = require("./database");
+const pool = require('./database');
 
 const create = (description) => {
-  pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [
+  pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [
     description,
   ]);
 };
 
 const get = () => {
-  pool.query("SELECT * FROM todo");
+  pool.query('SELECT * FROM todo');
 };
 
 const remove = (id) => {
-  pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+  pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
 };
 
 module.exports = {
